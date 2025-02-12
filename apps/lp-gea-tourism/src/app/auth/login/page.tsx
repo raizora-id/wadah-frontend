141577f0327462a5a1b8b9adc824377afe6f60b4
Ridho Assuryadi <ulunmuhammadridho@gmail.com>
2025-02-12 19:02:22 +0700

    feat: setup astro components

---
+++ apps/lp-gea-tourism/src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { SignInScreen } from '@/modules/auth/signin-screen';
+
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return <SignInScreen />;
+};
+
+export default Page;
---
+++ apps/lp-ketuju/src/data/site-metadata.js
@@ -0,0 +1,10 @@
+module.exports.siteMetadata = {
+    title: 'Ketuju Creative',
+    description: 'Dari Tantangan hingga Solusi Kreatif, Mitra Design dan Pengembangan Digital!',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://ketuju.com',
+    siteRepo: '',
+    socialBanner: '/images/social.png',
+    locale: 'en-US'
+};
