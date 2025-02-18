d2f4361f53548ba118658c0f7a09adc0fbca6b94
Ridho Assuryadi <ulunmuhammadridho@gmail.com>
2025-02-18 21:18:28 +0700

    feat: init footer raizora

--- apps/lp-gea-tourism/src/data/users.js
+++ apps/lp-gea-tourism/src/data/users.js
@@ -1,8 +1,8 @@
 export const users = [
-  {
-    id: "410544b2-4001-4271-9855-fec4b6a6442a",
-    name: "User",
-    email: "user@carhive.com",
-    password: "123456",
-  },
-]
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        name: 'User',
+        email: 'user@geatourism.com',
+        password: '123456'
+    }
+];
