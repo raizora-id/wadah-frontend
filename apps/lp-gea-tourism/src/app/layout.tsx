b64bc07c0cb5f1f378011a7c36ad4b1f5b2cef89
Ridho Assuryadi <ulunmuhammadridho@gmail.com>
2025-02-18 19:10:11 +0700

    feat: db schema setup

--- apps/lp-gea-tourism/src/app/layout.tsx
+++ apps/lp-gea-tourism/src/app/layout.tsx
@@ -1,74 +1,72 @@
-import type { Metadata, Viewport } from "next"
+import type { Metadata, Viewport } from 'next';
 
-import { Toaster } from "@/components/ui/toaster"
-
-import "../styles/globals.css"
-
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { SiteFooter } from "@/components/site-footer"
+import { SiteFooter } from '@/components/site-footer';
+import { Toaster } from '@/components/ui/toaster';
+import { siteConfig } from '@/config/site';
+import { fontSans } from '@/lib/fonts';
+import '@packages/ui/globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(siteConfig.url),
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  authors: {
-    name: siteConfig.author.name,
-    url: siteConfig.author.url,
-  },
-  creator: siteConfig.author.name,
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
+    metadataBase: new URL(siteConfig.url),
+    title: {
+        default: siteConfig.name,
+        template: `%s - ${siteConfig.name}`
+    },
     description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    creator: `@${siteConfig.author.name}`,
-  },
-  icons: {
-    icon: [
-      {
-        url: "/favicon.ico",
-        sizes: "32x32",
-      },
-      { url: "/icon.svg", type: "image/svg+xml" },
-      { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
-      { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
-    ],
-    apple: [{ url: "/apple-icon.png", type: "image/png" }],
-  },
-}
+    authors: {
+        name: siteConfig.author.name,
+        url: siteConfig.author.url
+    },
+    creator: siteConfig.author.name,
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: siteConfig.name,
+        description: siteConfig.description,
+        creator: `@${siteConfig.author.name}`
+    },
+    icons: {
+        icon: [
+            {
+                url: '/favicon.ico',
+                sizes: '32x32'
+            },
+            { url: '/icon.svg', type: 'image/svg+xml' },
+            { url: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+            { url: '/icon-512.png', type: 'image/png', sizes: '512x512' }
+        ],
+        apple: [{ url: '/apple-icon.png', type: 'image/png' }]
+    }
+};
 
 export const viewport: Viewport = {
-  themeColor: "white",
-  colorScheme: "light",
-  width: "device-width",
-  initialScale: 1,
-}
+    themeColor: 'white',
+    colorScheme: 'light',
+    width: 'device-width',
+    initialScale: 1
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+    children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-      <html lang="en" className={fontSans.variable}>
-        <body>
-          <div className="flex min-h-screen flex-col">
-            {children}
-            <SiteFooter />
-          </div>
-          <Toaster />
-        </body>
-      </html>
-  )
+    return (
+        <html lang='en' className={fontSans.variable}>
+            <body>
+                <div className='flex min-h-screen flex-col'>
+                    {children}
+                    <SiteFooter />
+                </div>
+                <Toaster />
+            </body>
+        </html>
+    );
 }
