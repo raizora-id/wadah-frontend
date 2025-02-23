f641f5a4c7d03e776415f983e2294b82dffd4a46
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-02-24 04:27:30 +0700

    feat: init klolakos

---
+++ apps/lp-klolakos/src/config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    name: 'GEA Tourism',
+    url: 'https://gea.raizora.com',
+    author: {
+        name: 'engineering raizora',
+        url: 'https://lab.raizora.com'
+    },
+    description:
+        'Explore the freedom of the open road with our premium car rental services! Discover a wide range of meticulously maintained vehicles, from sleek sedans to spacious SUVs, perfect for every occasion. With competitive prices, flexible rental options, and exceptional customer service, we make your journey unforgettable. Book your dream car today and embark on a seamless travel experience.',
+    links: {
+        github: 'https://github.com/raizora'
+    }
+};
+
+export type SiteConfig = typeof siteConfig;
