b892c99443e519de30545e459d343bab0cc3cf5f
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-02-28 05:10:23 +0700

    feat: add i18n

--- apps/ds-raizora/next.config.ts
+++ apps/ds-raizora/next.config.ts
@@ -1,9 +1,16 @@
+import type { NextConfig } from 'next';
+
 import initializeBundleAnalyzer from '@next/bundle-analyzer';
 import createConfig from '@tooling/nextjs-config';
 
-// https://www.npmjs.com/package/@next/bundle-analyzer
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 const withBundleAnalyzer = initializeBundleAnalyzer({
     enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
 });
 
-export default withBundleAnalyzer(createConfig({}));
+const nextConfig: NextConfig = createConfig({});
+
+export default withNextIntl(withBundleAnalyzer(nextConfig));
--- apps/ds-raizora/src/app/layout.tsx
+++ apps/ds-raizora/src/app/layout.tsx
@@ -10,6 +10,10 @@ import AuthProvider from '@/providers/session-provider';
 import { Toaster } from '@packages/ui/components/base/toaster';
 import '@packages/ui/globals.css';
 
+import { RefineContext } from './_refine_context';
+import { NextIntlClientProvider } from 'next-intl';
+import { getLocale, getMessages } from 'next-intl/server';
+
 const geistSans = localFont({
     src: './fonts/GeistVF.woff',
     variable: '--font-geist-sans',
@@ -27,12 +31,19 @@ export const metadata: Metadata = {
 };
 
 const Layout = async ({ children }: Readonly<{ children: ReactNode }>) => {
+    const locale = await getLocale();
+    const messages = await getMessages();
+
     return (
         <html suppressHydrationWarning lang='en'>
             <body className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}>
                 <ThemeProvider attribute='data-theme' defaultTheme='zycas'>
                     <ReactQueryProvider>
-                        <AuthProvider>{children}</AuthProvider>
+                        <NextIntlClientProvider locale={locale} messages={messages}>
+                            <RefineContext>
+                                <AuthProvider>{children}</AuthProvider>
+                            </RefineContext>
+                        </NextIntlClientProvider>
                     </ReactQueryProvider>
                 </ThemeProvider>
                 <Toaster />
