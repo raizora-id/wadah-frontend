78f357c880358115a13ed5ec41c7d1205c66d433
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-02-28 17:59:06 +0700

    fix: build klolakos landing page build

--- apps/lp-klolakos/src/app/page.tsx
+++ apps/lp-klolakos/src/app/page.tsx
@@ -1,9 +1,4 @@
-import { ChatBubble } from '@/components/chat-bubble';
-import { FAQCard } from '@/components/faq-card';
-import { FeatureCard } from '@/components/feature-card';
 import Home from '@/modules/home/home';
-import { Button } from '@packages/ui/src/components/base/button';
-import { Card } from '@packages/ui/src/components/base/card';
 
 export default function HomePage() {
     return <Home />;
