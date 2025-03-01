73de8804fef4c52faec6b9412a2a0aeb01e712a4
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-03-01 12:08:38 +0700

    feat: init landing page klolaform

--- apps/lp-klolaform/next.config.ts
+++
@@ -1,9 +0,0 @@
-import initializeBundleAnalyzer from '@next/bundle-analyzer';
-import createConfig from '@tooling/nextjs-config';
-
-// https://www.npmjs.com/package/@next/bundle-analyzer
-const withBundleAnalyzer = initializeBundleAnalyzer({
-    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
-});
-
-export default withBundleAnalyzer(createConfig({}));
--- apps/lp-klolaform/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres';
-
-import * as schema from './schema';
-import { config } from 'dotenv';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-
-config({ path: '.env' });
-
-export const db = drizzle(sql, { schema });
