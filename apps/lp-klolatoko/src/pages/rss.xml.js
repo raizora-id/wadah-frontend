62b5959d9d54df57b5ea3f8ef780c45ecdba0b34
Ridho <ridho@192.168.1.9>
2025-03-07 04:57:45 +0700

    feat: add favicons klolatoko

--- apps/lp-klolatoko/src/pages/rss.xml.js
+++ apps/lp-klolatoko/src/pages/rss.xml.js
@@ -1,8 +1,10 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import config from '../../site.config';
+
 export async function GET(context) {
   return rss({
-     title: 'Lexington Themes',
-    description: 'Free and premium multipage themes and UI Kits For freelancers, developers, businesses, and personal use.Beautifully crafted with Astro.js, and Tailwind CSS — Simple & easy to customise.',
+     title: config.title,
+    description: config.description,
     site: context.site,
     items: await pagesGlobToRssItems(
       import.meta.glob('./blog/*.{md,mdx}'),
