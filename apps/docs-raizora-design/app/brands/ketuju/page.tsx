f68a7029b1807955e90b013bad985cf4820e9f87
Ridho <ridho@192.168.1.7>
2025-03-11 07:29:21 +0700

    feat: add logo and favicon

---
+++ apps/docs-raizora-design/app/brands/ketuju/page.tsx
@@ -0,0 +1,20 @@
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+import { useMDXComponents } from '../../../mdx-components'
+import TypographyStyleGuide from './ _components/typography'
+
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
+ 
+
+ 
+const Wrapper = useMDXComponents().wrapper
+ 
+export default async function Page(props) {
+  const params = await props.params
+  const result = await importPage(params.mdxPath)
+  const { default: MDXContent, toc, metadata } = result
+  return (
+    <Wrapper toc={toc} metadata={metadata}>
+      <TypographyStyleGuide />
+    </Wrapper>
+  )
+}
---
+++ apps/docs-raizora-design/site.config.js
@@ -0,0 +1,11 @@
+export const title = 'Bungas | Raizora Design System';
+export const description = "Raizora Design System, Made for building consistent and delightful web experiences."
+export const url = 'design.raizora.com';
+
+const Config = {
+    title: title,
+    description: description,
+    url: url
+}
+
+export default Config
