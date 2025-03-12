7d4a6eddfd7ebf862b4c09b8d887656f947eb0a2
Ridho <ridho@192.168.1.7>
2025-03-13 04:44:16 +0700

    feat: add category bar

--- apps/lp-ketuju/src/components/ui/tooltip.tsx
+++ apps/lp-ketuju/src/components/ui/tooltip.tsx
@@ -27,4 +27,95 @@ const TooltipContent = React.forwardRef<
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
+
+
+
+
+interface TooltipProps
+  extends Omit<TooltipPrimitive.TooltipContentProps, "content" | "onClick">,
+    Pick<
+      TooltipPrimitive.TooltipProps,
+      "open" | "defaultOpen" | "onOpenChange" | "delayDuration"
+    > {
+  content: React.ReactNode
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  side?: "bottom" | "left" | "top" | "right"
+  showArrow?: boolean
+  triggerAsChild?: boolean
+}
+
+const TooltipChart = React.forwardRef<
+  React.ComponentRef<typeof TooltipPrimitive.Content>,
+  TooltipProps
+>(
+  (
+    {
+      children,
+      className,
+      content,
+      delayDuration,
+      defaultOpen,
+      open,
+      onClick,
+      onOpenChange,
+      showArrow = true,
+      side,
+      sideOffset = 10,
+      triggerAsChild = false,
+      ...props
+    }: TooltipProps,
+    forwardedRef,
+  ) => {
+    return (
+      <TooltipPrimitive.Provider delayDuration={150}>
+        <TooltipPrimitive.Root
+          open={open}
+          defaultOpen={defaultOpen}
+          onOpenChange={onOpenChange}
+          delayDuration={delayDuration}
+          tremor-id="tremor-raw"
+        >
+          <TooltipPrimitive.Trigger onClick={onClick} asChild={triggerAsChild}>
+            {children}
+          </TooltipPrimitive.Trigger>
+          <TooltipPrimitive.Portal>
+            <TooltipPrimitive.Content
+              ref={forwardedRef}
+              side={side}
+              sideOffset={sideOffset}
+              align="center"
+              className={cn(
+                // base
+                "max-w-60 select-none rounded-md px-2.5 py-1.5 text-sm leading-5 shadow-md",
+                // text color
+                "text-gray-50 dark:text-gray-900",
+                // background color
+                "bg-gray-900 dark:bg-gray-50",
+                // transition
+                "will-change-[transform,opacity]",
+                "data-[side=bottom]:animate-slideDownAndFade data-[side=left]:animate-slideLeftAndFade data-[side=right]:animate-slideRightAndFade data-[side=top]:animate-slideUpAndFade data-[state=closed]:animate-hide",
+                className,
+              )}
+              {...props}
+            >
+              {content}
+              {showArrow ? (
+                <TooltipPrimitive.Arrow
+                  className="border-none fill-gray-900 dark:fill-gray-50"
+                  width={12}
+                  height={7}
+                  aria-hidden="true"
+                />
+              ) : null}
+            </TooltipPrimitive.Content>
+          </TooltipPrimitive.Portal>
+        </TooltipPrimitive.Root>
+      </TooltipPrimitive.Provider>
+    )
+  },
+)
+
+Tooltip.displayName = "Tooltip"
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider, TooltipChart }
+
