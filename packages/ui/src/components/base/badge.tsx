06e7dbfd64b72e441e2e8d854b30bb5f6834d2e5
Ridho <ridho@192.168.1.7>
2025-03-14 05:50:54 +0700

    feat: customization badge components

--- packages/ui/src/components/base/badge.tsx
+++ packages/ui/src/components/base/badge.tsx
@@ -9,10 +9,29 @@ const badgeVariants = cva(
     {
         variants: {
             variant: {
-                default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
                 secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 destructive: 'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-                outline: 'text-foreground'
+                outline: 'text-foreground',
+                default: [
+                    "bg-blue-50 text-blue-900 ring-blue-500/30",
+                    "dark:bg-blue-400/10 dark:text-blue-400 dark:ring-blue-400/30",
+                  ],
+                  neutral: [
+                    "bg-gray-50 text-gray-900 ring-gray-500/30",
+                    "dark:bg-gray-400/10 dark:text-gray-400 dark:ring-gray-400/20",
+                  ],
+                  success: [
+                    "bg-emerald-50 text-emerald-900 ring-emerald-600/30",
+                    "dark:bg-emerald-400/10 dark:text-emerald-400 dark:ring-emerald-400/20",
+                  ],
+                  error: [
+                    "bg-red-50 text-red-900 ring-red-600/20",
+                    "dark:bg-red-400/10 dark:text-red-400 dark:ring-red-400/20",
+                  ],
+                  warning: [
+                    "bg-yellow-50 text-yellow-900 ring-yellow-600/30",
+                    "dark:bg-yellow-400/10 dark:text-yellow-500 dark:ring-yellow-400/20",
+                  ],
             }
         },
         defaultVariants: {
